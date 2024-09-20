c3657f9c38922946ba4a6c37195fbcf13424d2d9
Bear-Web-Dev <avramiwise@gmail.com>
2024-09-20 09:28:43 -0400

    first program

---
+++ firstprogram.py
@@ -0,0 +1,10 @@
+# Basic Calculator 
+
+print("hey fam, welcome to my bussin Calculator!")
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+
+answer = num1 + num2
+print(f"Your answer is: {answer} !!!")
+
+#
